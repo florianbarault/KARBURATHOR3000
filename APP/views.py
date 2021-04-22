3e3a62df8cec76b1d46cc2b5d5de3c46a82bd318
florianbarault <florianbarault@orange.fr>
2021-04-22 11:33:00 +0200

    Initial commit

---
+++ APP/views.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+from APP.data import bdd as b
+from APP.controller import formulaire as f
+
+app = Flask(__name__)
+app.static_folder = "static"
+app.template_folder = "template"
+app.config.from_object('config')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/new_route")
+def new_route():
+    return render_template("new_route.html")
---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = "TRUE"
+CACHE_TYPE = "null"
+SECRET_KEY = "dsfvgblozehd"
