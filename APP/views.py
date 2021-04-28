d5976ac8ce25ae94e61c1e650eaa5ca3fc5c6836
Rapsen <hippolyte.foulongne@gmail.com>
2021-04-28 15:03:03 +0200

    ajout template historique et commentaire

--- APP/views.py
+++ APP/views.py
@@ -7,14 +7,28 @@ app.static_folder = "static"
 app.template_folder = "template"
 app.config.from_object('config')
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
 @app.route("/new_route")
 def new_route():
     return render_template("new_route.html")
+
+
+@app.route("/historic")
+def historic():
+    return render_template("historic.html")
+
+
+@app.route("/comments")
+def comments():
+    return render_template("comments.html")
+
