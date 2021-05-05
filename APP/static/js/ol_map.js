ed5f771eeeb1cdbf893536ca73447a882eb016cd
florianbarault <florianbarault@orange.fr>
2021-05-05 16:46:00 +0200

    Ajout historique

--- APP/static/js/ol_map.js
+++ APP/static/js/ol_map.js
@@ -1,12 +1,12 @@
 var map = new ol.Map({
-        target: 'map',
+        target: 'ol_localisation',
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
           })
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
+          center: ol.proj.fromLonLat([2.4682543369163965, 46.8643629949555]),
+          zoom: 6
         })
       });
