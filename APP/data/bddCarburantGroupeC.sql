f04fe5d164579156fc5a750e01d688b8a33d2b62
florianbarault <florianbarault@orange.fr>
2021-05-07 13:51:25 +0200

    import de la bb corrigé

--- APP/data/bddCarburantGroupeC.sql
+++ APP/data/bddCarburantGroupeC.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
+CREATE DATABASE IF NOT EXISTS bddCarburantGroupeC;
 
 CREATE TABLE `aerodrome` (
   `OACI` varchar(5) NOT NULL,
