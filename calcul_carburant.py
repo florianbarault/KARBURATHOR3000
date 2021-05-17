f4f39c330067e6aadfe05fd433bdad6daeb98615
Js221 <75114606+Js221@users.noreply.github.com>
2021-05-17 16:41:07 +0200

    oui

---
+++ calcul_carburant.py
@@ -0,0 +1,34 @@
+from math import *
+#Raymer
+M4_M3=0.995
+M2_M1=0.985
+M1_M0=0.970
+g=9.81
+def conso_carbu(M0,M4_M0):
+    return 1.06*M0*(1-_M4_M0)
+
+def calcul_M4_M0(distance,conso_specifique,vitesse_croisiere,finesse):
+    return exp(-1*((distance*g*conso_specifique)/(vitesse_croisiere*finesse)))
+    
+def calcul_conso_spe(conso_horaire,puissance,rendement_helice):
+    return conso_horaire/(puissance*rendement_helice)
+
+def calcul_Mcarb_M0(M4_M0):
+    return 1.06*(1-M4_M0)
+
+def calcul_M0(Mvide,M_marchande,M_equipage,Mcar_M0,Mv_M0):
+    M0_i= 1000
+        
+        
+    Mv_M0= Mvide_M0(M0_i)
+    M0_v= int((M_marchande+M_equipage)/(1-Mcar_M0-Mv_M0))# masses à renseigner 
+    while M0_i != M0_v:
+            M0_i+=1
+            Mv_M0= Mvide_M0(M0_i)
+            M0_v= int((M_marchande+M_equipage)/(1-Mcar_M0-Mv_M0))
+        
+    return M0_i    
+carb_DR400=conso_carbu(M0_DR400,MO_M4_DR400)
+#MO_DR400=
+#MO_M4_DR400=
+
