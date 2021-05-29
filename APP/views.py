a36039046d469f1f9afa38ebb9135734ecb3a183
Rapsen <hippolyte.foulongne@gmail.com>
2021-05-29 17:32:13 +0200

    correctif erreur list index out of range

--- APP/views.py
+++ APP/views.py
@@ -134,17 +134,20 @@ def addflight():
         idVol = b.get_idVol(idUtilisateur)
         vol = idVol[0][0]
         etapes = request.form['etapes']
-        b.ajout_etapes(vol,etapes)
+        if len(etapes)<2:
+            return render_template("new_route.html", data=b.getaerodrome(), avion=b.getNomAvion(), info="pbEtape")
+        else:
+            b.ajout_etapes(vol,etapes)
 
-        #Calculs pour les estimations
+            #Calculs pour les estimations
 
-        dist, carb, coordonnees_generales = b.update_info(vol)
+            dist, carb, coordonnees_generales = b.update_info(vol)
 
-        #Data nécessaires pour la page recap
-        liste_etapes = b.get_etapes(vol)
-        data,conso_totale, dist_totale=b.conso_dist_etapes(liste_etapes,carb,dist)
+            #Data nécessaires pour la page recap
+            liste_etapes = b.get_etapes(vol)
+            data,conso_totale, dist_totale=b.conso_dist_etapes(liste_etapes,carb,dist)
 
-        return render_template("recap.html", table=data, coord_map=coordonnees_generales, conso_totale=conso_totale, dist_totale=dist_totale, info=session["statut"])
+            return render_template("recap.html", table=data, coord_map=coordonnees_generales, conso_totale=conso_totale, dist_totale=dist_totale, info=session["statut"])
     else:
         return redirect(url_for('/login'))
 
